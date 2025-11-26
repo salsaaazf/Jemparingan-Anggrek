b60e7f2f1a83a97663a0ab85c563eb8a03c6be0e
MACBOOK <macbook@MACBOOKs-MacBook-Pro-3.local>
2025-11-26 15:06:10 +0700

    commit arin

--- src/components/rekap-table.vue
+++ src/components/rekap-table.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, computed } from 'vue'
 import { useJemparinganStore } from '../stores/jemparinganStore.js';
 import { useAuthStore } from '@/stores/authStore.js';
 import { ArrowLeft } from 'lucide-vue-next';
@@ -9,6 +10,31 @@ const authStore = useAuthStore();
 const goBack = () => {
   authStore.role = 'committee';
 };
+
+const currentPage = ref(1);
+const rowsPerPage = ref(10);     
+
+const totalPages = computed(() => {
+  return Math.ceil(jemparinganStore.finalRecap.length / rowsPerPage.value);
+});
+
+const paginatedData = computed(() => {
+  const start = (currentPage.value - 1) * rowsPerPage.value;
+  const end = start + rowsPerPage.value;
+  return jemparinganStore.finalRecap.slice(start, end);
+});
+
+const handleChangePage = (page) => {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page;
+  }
+};
+
+const handleRowPageChanges = (newValue) => {
+  if(currentPage.value!=1){
+    handleChangePage(1);
+  }
+}
 </script>
 
 <template>
@@ -39,8 +65,8 @@ const goBack = () => {
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(player, index) in jemparinganStore.finalRecap" :key="player.nomorPemanah">
-            <td>{{ index + 1 }}</td> 
+            <tr v-for="(player, index) in paginatedData" :key="player.nomorPemanah">
+            <td>{{ (currentPage-1)*rowsPerPage + index +1 }}</td>
             <td>{{ player.nama }}</td>
             <td>{{ player.red }}</td>
             <td>{{ player.white }}</td>
@@ -49,8 +75,47 @@ const goBack = () => {
           </tbody>
         </table>
       </div>
+      
+      <div class="pagination-container">
+        <div class="pagination-button">
+          <button class="pg-btn pg-arrow" 
+            @click="handleChangePage(currentPage - 1)"
+            :disabled="currentPage === 1">
+            ‹
+          </button>
+
+          <button class="pg-btn"
+            v-for="page in totalPages"
+            :key="page"
+            :class="{ active: currentPage === page }"
+            @click="handleChangePage(page)">
+            {{ page }}
+          </button>
+
+          <button class="pg-btn pg-arrow"
+            @click="handleChangePage(currentPage + 1)"
+            :disabled="currentPage === totalPages">
+            ›
+          </button>
+        </div>
+        
+        <div class="showSubmit">
+          <div class="showRow">
+          <p class="show"> <strong>Show:</strong></p>
+        <select v-model="rowsPerPage" class="row-select" @change="handleRowPageChanges">
+          <option :value="10">10 rows</option>
+          <option :value="20">20 rows</option>
+          <option :value="50">50 rows</option>
+        </select>
+        </div>
+        <button class="submitBtn">
+          Submit Data
+        </button>
+        </div>
+        
+        
+      </div>
   </div>
-  
 </template>
 
 <style scoped>
@@ -166,5 +231,83 @@ tbody tr:last-child td:first-child {
 
 tbody tr:last-child td:last-child {
   border-bottom-right-radius: 15px;
+}
+
+.pagination-container {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 10px;
+}
+
+.pagination-button {
+    display: flex; 
+    align-items: center; 
+    gap: 8px; 
+}
+
+.pg-btn {
+  padding: 6px 10px;
+  border: none;
+  background: #F5F5F5;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.pg-btn.pg-arrow {
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); 
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.pg-btn.active {
+  background: #355F56;
+  color: white;
+  font-weight: bold;
+}
+
+.pg-btn:disabled {
+  opacity: 0.4;
+  cursor: not-allowed;
+}
+
+.showSubmit{
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  gap: 5px;
+  margin-left: auto;
+}
+
+.show{
+  margin: 0px;
+}
+
+.showRow {
+  background: #F5F5F5;
+  border-radius: 5px;
+  border: none;
+  display: flex;
+  align-items: center;
+  padding: 6px 10px;
+}
+
+.row-select{
+  height: 40px;
+  border: none;
+  background: #F5F5F5;
+}
+
+.submitBtn{
+  background-color: #3B5D50;
+  color: white;
+  width: 130px;
+  border-radius: 5px;
+  border: none;
+  padding-inline: 7px;
+  padding-block: 5px;
+  text-align: center;
+  cursor: pointer;
+  
 }
 </style>
--- src/stores/jemparinganStore.js
+++ src/stores/jemparinganStore.js
@@ -4,8 +4,44 @@ import { KeyRound, Target } from 'lucide-vue-next';
 
 export const useJemparinganStore = defineStore('jemparingan', {
   state: () => ({
-    redHits: [{ nomorPemanah: 1, nama: 'Pemanah Uji Coba' },], 
-    whiteHits: [],
+    redHits: [  { nomorPemanah: 1, nama: 'Pemanah 1' }, //ini semua cuma dummy biar akuu bisa lihat hasilnya keluar ga ditable
+  { nomorPemanah: 2, nama: 'Pemanah 2' },
+  { nomorPemanah: 3, nama: 'Pemanah 3' },
+  { nomorPemanah: 4, nama: 'Pemanah 4' },
+  { nomorPemanah: 5, nama: 'Pemanah 5' },
+  { nomorPemanah: 6, nama: 'Pemanah 6' },
+  { nomorPemanah: 7, nama: 'Pemanah 7' },
+  { nomorPemanah: 8, nama: 'Pemanah 8' },
+  { nomorPemanah: 9, nama: 'Pemanah 9' },
+  { nomorPemanah: 10, nama: 'Pemanah 10' },
+  { nomorPemanah: 11, nama: 'Pemanah 11' },
+  { nomorPemanah: 12, nama: 'Pemanah 12' },
+  { nomorPemanah: 13, nama: 'Pemanah 13' },
+  { nomorPemanah: 14, nama: 'Pemanah 14' },
+  { nomorPemanah: 15, nama: 'Pemanah 15' },
+  { nomorPemanah: 16, nama: 'Pemanah 16' },
+  { nomorPemanah: 17, nama: 'Pemanah 17' },
+  { nomorPemanah: 18, nama: 'Pemanah 18' },], 
+    whiteHits: [  { nomorPemanah: 1, nama: 'Pemanah 1' }, //ini semua cuma dummy biar akuu bisa lihat hasilnya keluar ga ditable
+  { nomorPemanah: 2, nama: 'Pemanah 2' },
+  { nomorPemanah: 3, nama: 'Pemanah 3' },
+  { nomorPemanah: 4, nama: 'Pemanah 4' },
+  { nomorPemanah: 5, nama: 'Pemanah 5' },
+  { nomorPemanah: 7, nama: 'Pemanah 7' },
+  { nomorPemanah: 8, nama: 'Pemanah 8' },
+  { nomorPemanah: 9, nama: 'Pemanah 9' },
+  { nomorPemanah: 10, nama: 'Pemanah 10' },
+  { nomorPemanah: 11, nama: 'Pemanah 11' },
+  { nomorPemanah: 12, nama: 'Pemanah 12' },
+  { nomorPemanah: 13, nama: 'Pemanah 13' },
+  { nomorPemanah: 15, nama: 'Pemanah 15' },
+  { nomorPemanah: 16, nama: 'Pemanah 16' },
+  { nomorPemanah: 17, nama: 'Pemanah 17' },
+  { nomorPemanah: 18, nama: 'Pemanah 18' },
+  { nomorPemanah: 19, nama: 'Pemanah 19' },
+  { nomorPemanah: 20, nama: 'Pemanah 20' },
+  { nomorPemanah: 21, nama: 'Pemanah 21' },
+  { nomorPemanah: 22, nama: 'Pemanah 22' },], //dummy selesai
 
     matchInfo: {
       round: '_',
